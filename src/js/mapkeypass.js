cac3bc09672ba1a739f39bd4d13aa3ddcfa50710
Marvin Conley <marvinlcon@gmail.com>
2018-12-27 21:59:15 -0600

    Enable MapKey Pass

---
+++ src/js/mapkeypass.js
@@ -0,0 +1,7 @@
+ // js/mapkeypass.js
+export default class MAPKEYPASS {
+    static get mapbox_key() {
+      // return your mapbox API key
+      return "pk.eyJ1IjoibWFydmNvbiIsImEiOiJjamljZzdvNmcwMG8wM2txdjFsZnNhanNqIn0.ienXnxJURNO-1UFDdagzqg";
+    }
+  }
